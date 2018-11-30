fb120c9ddec9313642a4af2435e9e512f30e9aa6
Bahrudin Hrnjica <bhrnjica@hotmail.com>
2018-11-30 08:53:07 +0100

    Image preparation UI

--- src/tool/mldatapreparation.wnd/ImageClassificator.xaml.cs
+++ src/tool/mldatapreparation.wnd/ImageClassificator.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,19 +17,42 @@ using System.Windows.Shapes;
 
 namespace DataProcessing.Wnd
 {
+    
     /// <summary>
     /// Interaction logic for ImageClassificator.xaml
     /// </summary>
     public partial class ImageClassificator : UserControl
     {
+        ObservableCollection<ImageLabelItem> Labels { get; set; }
         public ImageClassificator()
         {
             InitializeComponent();
+            Labels = new ObservableCollection<ImageLabelItem>();
+            imageLabelList.ItemsSource = Labels;
+        }
+
+        private void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+            Labels.Add(new ImageLabelItem());
+        }
+
+        private void Button_Click_Remove(object sender, RoutedEventArgs e)
+        {
+            if(imageLabelList.SelectedItem!=null && MessageBoxResult.Yes == MessageBox.Show("Confirm deletion", "ANNdotNET", MessageBoxButton.YesNo))
+             Labels.Remove((ImageLabelItem)imageLabelList.SelectedItem);
         }
 
         private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            var textBox = sender as TextBox;
+            e.Handled = !Regex.IsMatch(e.Text, @"^[0-9]*(?:\.[0-9]*)?$");
         }
+    }
+
+    public class ImageLabelItem
+    {
+        public string Label { get; set; }
+        public string Folder { get; set; }
+        public string Query { get; set; }
     }
 }
---
+++ src/tool/mldatapreparation.wnd/ImageItem.xaml.cs
@@ -0,0 +1,39 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////
+// ANNdotNET - Deep Learning Tool on .NET Platform                                      //
+// Copyright 2017-2018 Bahrudin Hrnjica                                                 //
+//                                                                                      //
+// This code is free software under the MIT License                                     //
+// See license section of  https://github.com/bhrnjica/anndotnet/blob/master/LICENSE.md //
+//                                                                                      //
+// Bahrudin Hrnjica                                                                     //
+// bhrnjica@hotmail.com                                                                 //
+// Bihac, Bosnia and Herzegovina                                                        //
+// http://bhrnjica.net                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DataProcessing.Wnd
+{
+    /// <summary>
+    /// Interaction logic for ItemItem.xaml
+    /// </summary>
+    public partial class ImageItem : UserControl
+    {
+        public ImageItem()
+        {
+            InitializeComponent();
+           // this.DataContextChanged += LayerItem_DataContextChanged;
+        }
+
+       
+        private void folderButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.ShowDialog();
+            txtCdim.Text = dialog.SelectedPath;
+        }
+    }
+}
