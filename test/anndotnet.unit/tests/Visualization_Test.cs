9faafdc5d5d43cab0eb5464cd8bbecd83993e42e
Bahrudin Hrnjica <bhrnjica@hotmail.com>
2018-12-10 11:28:45 +0100

    Network Graph code improvements

--- test/anndotnet.unit/tests/Visualization_Test.cs
+++ test/anndotnet.unit/tests/Visualization_Test.cs
@@ -19,6 +19,7 @@ namespace anndotnet.unit
         public void export_to_csv_regression_test01()
         {
             DeviceDescriptor device = DeviceDescriptor.UseDefaultDevice();
+            //var strPath = "C:\\sc\\Datasets\\ResNet18_ImageNet_CNTK.model";
             var strPath = "C:\\sc\\github\\anndotnet\\src\\tool\\anndotnet.wnd\\Resources\\Iris\\IrisProject\\FFModel\\models\\model_at_983of1000_epochs_TimeSpan_636729018198856673";
             vizualizeModel(strPath);
             //
