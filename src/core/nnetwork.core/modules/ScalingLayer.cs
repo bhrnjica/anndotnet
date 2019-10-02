b0a0c07ee65a12ef77ec519e76bf152aceff69ef
Bahrudin Hrnjica <bhrnjica@bahr-sb2.com>
2019-10-02 18:16:29 +0200

    Added more scaling layers

--- src/core/nnetwork.core/modules/ScalingLayer.cs
+++ src/core/nnetwork.core/modules/ScalingLayer.cs
@@ -10,19 +10,10 @@
 // Bihac, Bosnia and Herzegovina                                                          //
 // http://bhrnjica.net                                                                  //
 //////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
-// ANNdotNET - Deep Learning Tool                                                       //
-// Copyright 2017-2018 Bahrudin Hrnjica                                                 //
-//                                                                                      //
-// This code is free software under the MIT License                                     //
-// See license section of  https://github.com/bhrnjica/anndotnet/blob/master/LICENSE.md  //
-//                                                                                      //
-// Bahrudin Hrnjica                                                                     //
-// bhrnjica@hotmail.com                                                                 //
-// Bihac, Bosnia and Herzegovina                                                         //
-// http://bhrnjica.net                                                                  //
-//////////////////////////////////////////////////////////////////////////////////////////
 using CNTK;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace NNetwork.Core.Network.Modules
 {
     /// <summary>
@@ -43,5 +34,64 @@ namespace NNetwork.Core.Network.Modules
             var net  = (Variable)CNTK.CNTKLib.ElementTimes(scaleFactor, x);
             return net;
         }
+
+
+        public static List<Function> ZScore(List<Variable> xVars, Dictionary<string, List<List<float>>> X, Dictionary<string, (float[] mean, float[] invStd)> stats, DeviceDescriptor device)
+        {
+            //
+            var normalizedInputs = new List<Function>();
+            foreach (var input in xVars)
+            {
+                var seqName = stats.Where(x => x.Value.mean.Length == input.Shape.Dimensions.Last()).Select(x => x.Key).FirstOrDefault();
+
+                //
+                var nd_mean = new NDArrayView(new int[] { stats[seqName].mean.Length }, stats[seqName].mean, device);
+                var nd_invStd = new NDArrayView(new int[] { stats[seqName].invStd.Length }, stats[seqName].invStd, device);
+                var m = new Constant(nd_mean, "mean");
+                var s = new Constant(nd_invStd, "invStd");
+
+                //
+                var dif = CNTKLib.Minus(input, m);
+                var fn = CNTKLib.ElementTimes(s, dif);
+
+                //
+                var normalizedinput = CNTKLib.PerDimMeanVarianceNormalize(input, nd_mean, nd_invStd, input.Name + "_norm");
+                //
+                normalizedInputs.Add(normalizedinput);
+
+            }
+
+            return normalizedInputs;
+        }
+
+        public static List<Function> MinMax(List<Variable> xVars, Dictionary<string, List<List<float>>> X, Dictionary<string, (float[] min, float[] max_minInv)> stats, DeviceDescriptor device)
+        {
+            //
+            var normalizedInputs = new List<Function>();
+            foreach (var input in xVars)
+            {
+                var seqName = stats.Where(x => x.Value.min.Length == input.Shape.Dimensions.Last()).Select(x => x.Key).FirstOrDefault();
+
+                //
+                var nd_min = new NDArrayView(new int[] { stats[seqName].min.Length }, stats[seqName].min, device);
+                var nd_maxminInv = new NDArrayView(new int[] { stats[seqName].max_minInv.Length }, stats[seqName].max_minInv, device);
+
+                var m = new Constant(nd_min, "min");
+                var s = new Constant(nd_maxminInv, "maxminInv");
+
+                //
+                var dif = CNTKLib.Minus(input, m);
+                var fn = CNTKLib.ElementTimes(s, dif);
+
+                //
+                //var normalizedinput = CNTKLib.PerDimMeanVarianceNormalize(input, nd_mean, nd_std, input.Name+"_norm");
+                //
+                normalizedInputs.Add(fn);
+
+            }
+
+            return normalizedInputs;
+        }
+
     }
 }
